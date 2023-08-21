7eea10b75b2f4e26b9f0598615bcd7923113c607
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-20 23:03:57 -0500

    chore: extend tailwind colors pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,53 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         pinkest: "#f9cdcd",
         blackest: "#252422",
         whitest: "#f4f4f4",
       },
       fontFamily: {
         Montserrat: ['"Montserrat"', "sans-serif"],
-      },
-      backgroundImage: {
-        noise: 'url("/noise_background.png")',
-      },
-      animation: {
-        noiseShake: {
-          "0%": {
-            transform: "translate3d(0, 9rem, 0)",
-          },
-          "10%": {
-            transform: "translate3d(-1rem, -4rem, 0)",
-          },
-          "20%": {
-            transform: "translate3d(-8rem, 2rem, 0)",
-          },
-          "30%": {
-            transform: "translate3d(9rem, -9rem, 0)",
-          },
-          "40%": {
-            transform: "translate3d(-2rem, 7rem, 0)",
-          },
-          "50%": {
-            transform: "translate3d(-9rem, -4rem, 0)",
-          },
-          "60%": {
-            transform: "translate3d(2rem, 6rem, 0)",
-          },
-          "70%": {
-            transform: "translate3d(7rem, -8rem, 0)",
-          },
-          "80%": {
-            transform: "translate3d(-9rem, 1rem, 0)",
-          },
-          "90%": {
-            transform: "translate3d(6rem, -5rem, 0)",
-          },
-          to: {
-            transform: "translate3d(-7rem, 0, 0)",
-          },
-        },
       },
     },
   },

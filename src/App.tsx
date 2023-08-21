164845aa81773c7da7ab7ef6708fd037177033cd
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-20 20:33:20 -0500

    style: add animations and colors to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import Main from "./components/ui/Main/Main";
 function App() {
   return (
     <>
+      <Noise />
       <Header />
       <Main />
       <Contact />
     </>
   );
 }
+
+const Noise = () => {
+  return (
+    <div
+      id="noise"
+      className="fixed pointer-events-none inset w-full h-full"
+    ></div>
+  );
+};
 
 export default App;

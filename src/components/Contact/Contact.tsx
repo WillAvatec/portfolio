b8bab4431b0c99ee0235244ca53655dd09097f8a
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-28 22:28:32 -0500

    feat: new Contact component style

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,38 +1,55 @@
+import { PropsWithChildren } from "react";
+import NoiseBg from "../../assets/noise_background.png";
+
 function Contact() {
+  const style = { backgroundImage: `url(${NoiseBg})` };
   return (
-    <div className="contact bg-gray-950 p-4 text-white">
-      <h2 className="text-center text-2xl font-bold font-Montserrat underline pb-5">
-        Contact me
-      </h2>
-      <div className="flex flex-col gap-4">
-        <p className="font-semibold text-gray-100">
-          Please get in touch if you think our work could be mutually
-          beneficial!
-        </p>
-
+    <section className="bg-black text-white font-Montserrat p-16" style={style}>
+      <Email />
+      <div className="flex flex-row gap-20">
         <div>
-          <p>1234 Random Road</p>
-          <p>Random Town, California 12345</p>
+          <h5 className="text-gray-500">Contact Details</h5>
+          <p> +51 991586055</p>
         </div>
-
         <div>
-          <p>555-555-5555</p>
-          <p>ashleywilliams.is.not.real@gmail.com</p>
+          <h5 className="text-gray-500">Socials</h5>
+          <ul>
+            <A href=""> LinkedIn</A>
+            <A href=""> Facebook</A>
+            <A href=""> Instagram</A>
+            <A href=""> Behance</A>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-gray-500">Location</h5>
+          <p>Peru</p>
         </div>
       </div>
-
-      <div className="contact--external-links flex flex-row w-full">
-        <img
-          className="w-7"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-        />
-        <img
-          className="w-7"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
-        />
-      </div>
-    </div>
+    </section>
   );
 }
+
+const Email = () => (
+  <a className="flex flex-row gap-5 mb-10 max-w-min" href="#about">
+    <h2 className="text-7xl font-bold underline uppercase">
+      avatec2312@gmail.com
+    </h2>
+    <div className="relative w-16 h-16 aspect-square">
+      <div className="absolute top-0 right-0 bg-white w-3/4 h-3" />
+      <div className="absolute right-0 bg-white w-3 h-3/4" />
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 -rotate-45 bg-white w-full h-3" />
+    </div>
+  </a>
+);
+
+interface AProps extends PropsWithChildren {
+  href: string;
+}
+
+const A = ({ href, children }: AProps) => (
+  <a target="_blank" href={href} className="uppercase pr-7">
+    {children}
+  </a>
+);
 
 export default Contact;

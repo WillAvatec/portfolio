15f823e9a90a2b7a31609c3a2221684c7b08cbad
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-01 10:57:56 -0500

    feat: new Footer Component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="relative bottom-0 text-center text-xs p-4 pb-0 z-50 w-full mt-16 text-whitest">
+      <p>© Copyright 2023, Emanuel Llontop</p>
+      <a
+        href="https://github.com/WillAvatec/portfolio"
+        aria-label="github portfolio"
+        className="underline"
+      >
+        Checkout, the source code of this website.
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;

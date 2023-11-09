c8a4599256116d336b0a808ac02ab6a6f297dbc9
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-08 19:26:33 -0500

    chore: Add motivational image to AboutMe Section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
 import SkillsCarousel from "../SkillsCarousel/SkillsCarousel";
+import Passion from "../../assets/passion.jpg";
 
 function About() {
   return (
@@ -6,14 +7,14 @@ function About() {
       <div className="relative bg-black pointer-events-none select-none text-white w-[105%] py-2 mb-10">
         <SkillsCarousel />
       </div>
-      <div className="w-[1250px] max-w-full mx-auto px-16 py-12">
+      <div className="w-[1250px] max-w-full mx-auto sm:px-16 sm:py-12 px-6 py-8">
         <div className="flex flex-row items-center gap-5 mb-8">
           <h3 className="text-3xl underline decoration-4  text-orange-600">
             About Me
           </h3>
           <div className="w-60 h-[4px] bg-orange-600"></div>
         </div>
-        <div className="flex flex-col md:flex-row  gap-12">
+        <div className="flex flex-col md:flex-row  gap-12 py-8">
           <Left />
           <Right />
         </div>
@@ -63,7 +64,11 @@ const Left = () => {
 };
 
 const Right = () => {
-  return <div className="flex-1"></div>;
+  return (
+    <div className="flex-1">
+      <img src={Passion} alt="motivational phrase" />
+    </div>
+  );
 };
 
 export default About;
